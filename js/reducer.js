989946e7db1215c50d7185c6f8b56bcc64c259d5
github username <github email address>
2020-10-18 14:11:36 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -6,6 +6,14 @@ function changeState(state, action){
       return state;
   }
 }
+function render(){
+  document.body.textContent = state.count
+}
+
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
 
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
